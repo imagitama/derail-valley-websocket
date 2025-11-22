e16cd931d0e842b2005213fc8b962e42924a2b12
Jared Williams <accounts@jaredwilliams.com.au>
2025-11-22 20:00:06 +0800

    Fallback to port and abs speed

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 1.0.1
+
+- changed car speedo to use UI if available, otherwise use port, otherwise use absolute speed
+
+# 1.0.0
+
+Initial version
--- DerailValleyWebSocket/CarHelper.cs
+++ DerailValleyWebSocket/CarHelper.cs
@@ -1,5 +1,8 @@
 using DV.HUD;
 using DV.Utils;
+using DV.Customization;
+using DV.Customization.Gadgets;
+using UnityEngine;
 
 namespace DerailValleyWebSocket;
 
@@ -13,9 +16,38 @@ public static class CarHelper
         return PlayerManager.Car.carLivery.parentType.id;
     }
 
-    public static float? GetCarSpeedometerValueKph()
+    public static float? CarSpeedometerValueKphFromUi()
     {
         var speed = SingletonBehaviour<HUDInterfacer>.Instance?.controlsManager?.indicatorReader?.speed?.Value;
         return speed;
+    }
+
+    public static float? GetCarSpeedometerValueKph()
+    {
+        var valueFromUi = CarSpeedometerValueKphFromUi();
+
+        if (valueFromUi != null)
+            return valueFromUi;
+
+        float? speedKph = null;
+
+        var simController = PlayerManager.Car.SimController;
+        var simulationFlow = simController.SimulationFlow;
+
+        var tractionPortsFeeder = simController.tractionPortsFeeder;
+
+        var portId = tractionPortsFeeder.forwardSpeedPortId;
+
+        if (simulationFlow.TryGetPort(portId, out var port))
+        {
+            var speed = port.Value;
+            speedKph = Mathf.Abs(speed);
+        }
+        else if (PlayerManager.Car != null)
+        {
+            speedKph = PlayerManager.Car.GetAbsSpeed() * 3.6f;
+        }
+            
+        return speedKph;
     }
 }
